5d61d48de88545f945f9e82aca7828058071eba5
syedajmal-a <mailtosyedajmal.a@gmail.com>
2026-02-18 10:21:52 +0530

    Odd Squad Counter (Count Odd Numbers).c

---
+++ Odd Squad Counter (Count Odd Numbers).c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    int arr[1000];
+    int count = 0;
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+
+        if(arr[i] % 2 != 0) {   // Check for odd number
+            count++;
+        }
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
