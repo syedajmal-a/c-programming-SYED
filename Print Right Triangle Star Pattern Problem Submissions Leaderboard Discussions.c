ec25f16f584a47ac60b721dac8e8777709d1546d
syedajmal-a <mailtosyedajmal.a@gmail.com>
2026-02-19 09:35:47 +0530

    Print Right Triangle Star Pattern Problem Submissions Leaderboard Discussions.c

---
+++ Print Right Triangle Star Pattern Problem Submissions Leaderboard Discussions.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int N;
+    scanf("%d", &N);
+
+    for(int i = 1; i <= N; i++) {
+        for(int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
